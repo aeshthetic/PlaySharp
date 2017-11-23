a997a44c7d77ecd868bfca447cab551e1cc361cc
aeshthetic <eabhijay@protonmail.com>
2017-11-23 17:25:50 -0500

    Added arg parsing and updated config template

--- src/Downloader/Player.fs
+++ src/Downloader/Player.fs
@@ -1,9 +1,20 @@
 ﻿open System
 open Scraper
 open FSharp.Data
+open Argu
 
 type Config = JsonProvider<"config.json">
-let config = Config.Load("config.json")
+
+type Args =
+    | [<AltCommandLine("-c")>] ConfigPath of path: string
+    | PlayMode of mode: string
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | ConfigPath _ -> "Specify the path of a config file"
+            | PlayMode _ -> "Specify a play mode [autoplay or manual]"
+
 let rec displayList choice (choices: Song list) =
     // Traverses a list of results incrementing the associated number every call, printing each result with its associated number
     match choices with
@@ -38,7 +49,7 @@ let exec (cmdString: string) =
     executableProcess.Start() |> ignore
     executableProcess.StandardOutput.ReadToEnd()
 
-let downloadPlay ffmpegBin song =
+let downloadPlay (config: Config.Root) ffmpegBin song =
     exec ("youtube-dl --extract-audio --audio-format vorbis --output \""+config.DownloadPath+(song.name |> removeSpaces)+".%(ext)s\" \"" + song.url + "\"") |> ignore
     exec (sprintf "%s -loglevel quiet -i %s -acodec libmp3lame %s.mp3" ffmpegBin (sprintf "%s%s.ogg" config.DownloadPath (song.name |> removeSpaces)) (config.DownloadPath + (song.name |> removeSpaces))) |> ignore
     song.name
@@ -47,17 +58,17 @@ let downloadPlay ffmpegBin song =
     |> exec
     |> ignore
 
-let rec playOnline played autoLimit (initialSong: Song) =
+let rec playOnline (config: Config.Root) played autoLimit (initialSong: Song) =
     // Recursively downloads and plays a song (and suggested songs) until $autoLimit songs have been played
     let ffmpegBin = config.FfmpegBinary
     if played >= autoLimit then
         0
     else
         printfn "Downloading and playing %s..." (initialSong.name)
-        downloadPlay ffmpegBin initialSong
-        playOnline (played + initialSong.duration) autoLimit (initialSong |> findNext)
+        downloadPlay config ffmpegBin initialSong
+        playOnline config (played + initialSong.duration) autoLimit (initialSong |> findNext)
 
-let rec playList played limit songs =
+let rec playList (config: Config.Root) played limit songs =
     let ffmpegBin = config.FfmpegBinary
     if played >= limit then
         0
@@ -65,11 +76,11 @@ let rec playList played limit songs =
         match songs with
         | [] -> 0
         | [song] -> 
-            downloadPlay ffmpegBin song
+            downloadPlay config ffmpegBin song
             0
         | song :: rest ->
-            downloadPlay ffmpegBin song
-            playList (played + song.duration) limit rest
+            downloadPlay config ffmpegBin song
+            playList config (played + song.duration) limit rest
 
 let rec makeList duration max songs =
     if duration <= max then
@@ -88,14 +99,14 @@ let rec makeList duration max songs =
         | None -> songs
     else
         songs
-let manualPlay () =
+let manualPlay (config: Config.Root) =
     printf "How long should music play? (HH:MM:SS) "
     let duration = Console.ReadLine() |> TimeSpan.Parse
     makeList ("00:00:00" |> TimeSpan.Parse) duration []
     |> List.rev
-    |> playList ("00:00:00" |> TimeSpan.Parse) duration
+    |> playList config ("00:00:00" |> TimeSpan.Parse) duration
        
-let autoPlay () =
+let autoPlay (config: Config.Root) =
     let searchTerm = 
         match config.FirstSearch with
         | "BLANK" -> 
@@ -119,22 +130,27 @@ let autoPlay () =
         Console.ReadLine() |> TimeSpan.Parse
 
     match song with
-    | Some(songItem) -> playOnline ("00:00:00" |> TimeSpan.Parse) limit songItem
+    | Some(songItem) -> playOnline config ("00:00:00" |> TimeSpan.Parse) limit songItem
     | None -> 1
 
 [<EntryPoint>]
 let main argv =
+    let parser = ArgumentParser.Create<Args>(programName = "PlaySharp")
+    let args = parser.Parse argv
+    let config =
+        match args.TryGetResult <@ ConfigPath @> with
+        | Some (path) -> Config.Load(path)
+        | None -> Config.Load("config.json")
+
+    let mode =
+        match args.TryGetResult <@ PlayMode @> with
+        | Some (mode) -> mode
+        | None -> config.ModeDefault
+
     if not (System.IO.Directory.Exists config.DownloadPath) then
         System.IO.Directory.CreateDirectory(config.DownloadPath) |> ignore
     // Gets search results from arguments and allows the user to choose one, then continues to look up its url and plays the url if it exists
-    match (argv |> Array.toList) with
-    | [] ->
-        match config.ModeDefault with
-        | "autoplay" -> autoPlay ()
-        | "manual" -> manualPlay ()
-        | _ -> 1
-    | _ :: mode :: _ ->
-        match mode with
-        | "autoplay" -> autoPlay ()
-        | "manual" -> manualPlay ()
-        | _ -> 1
+    match mode with
+    | "autoplay" -> autoPlay config
+    | "manual" -> manualPlay config
+    | _ -> 1
